d55169bb974b762f4d3a17552316f1b675b5a71b
BenjaminMusili97 <160472861+Benjamin9999999@users.noreply.github.com>
2024-06-15 13:34:15 +0300

    Update Main.java

--- src/main/java/DataStructures/Main.java
+++ src/main/java/DataStructures/Main.java
@@ -1,17 +1,39 @@
-package DataStructures;
+package wt;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class SelectionSort {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+            int[] nums = {15, 2, 16, 14, 3, 13, 4, 12, 5, 11, 6, 10, 7, 9, 8, 1};
+            int size = nums.length;
+            int temp =0;
+            int minIndex=-1;
+            int maxIndex=-1;
+            System.out.println("Before sorting: " );
+            for (int num : nums) {
+                System.out.print(num + " ");
+            }
+            for (int i=0; i<size; i++){
+                minIndex=i;
+                for(int j=i+1; j<size; j++){
+                    if(nums[j]<nums[minIndex]){
+                        minIndex=j;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+                    }
+                    temp= nums[minIndex];
+                    nums[minIndex]= nums[i];
+                    nums[i]= temp;
+
+
+
+                }
+            }
+
+
+
+                System.out.println();
+                System.out.println("After Sorting: ");
+                for (int num : nums) {
+                    System.out.print( num+" ");
+                }
+
+            }
         }
-    }
-}
